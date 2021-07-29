aa2341e04645f54e2c1a4d852932557bb064dab3
unknown <rlucila438@gmail.com>
2021-07-29 13:28:00 -0300

    estructura del home

--- app.js
+++ app.js
@@ -5,3 +5,4 @@ const port = 3030;
 
 app.use(express.static('public'));
 app.listen (port, ()=> console.log(`Servidor corriendo en el puerto ${port}`))
+app.get ('/home', (req, res)=> res.sendFile(path.join(__dirname, 'views','home.html')))
