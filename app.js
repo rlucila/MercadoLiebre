b2b9236a7b0f7ae6f38939a6e81eb8553122aa43
unknown <rlucila438@gmail.com>
2021-07-29 11:46:17 -0300

    levanta servidor web

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require ('express')
+const app = express();
+const path = require('path')
+const port = 3030;
+
+app.use(express.static('public'));
+app.listen (port, ()=> console.log(`Servidor corriendo en el puerto ${port}`))
