2f82051aca31db49bf6b5b51b778da65f0427fbf
rlucila <rlucila438@gmail.com>
2021-09-01 23:39:23 -0300

    agrega formularios

--- app.js
+++ app.js
@@ -6,3 +6,4 @@ const port = 3030;
 app.use(express.static('public'));
 app.listen (port, ()=> console.log(`Servidor corriendo en el puerto ${port}`))
 app.get ('/home', (req, res)=> res.sendFile(path.join(__dirname, 'views','home.html')))
+app.get ('/register', (req, res)=> res.sendFile(path.join(__dirname, 'views','home.html')))
